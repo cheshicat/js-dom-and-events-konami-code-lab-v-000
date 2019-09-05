12a36ef190dfea70c1b9e0d9dbc1361f6c236e21
Allison Howard <alliemhow@gmail.com>
2019-09-05 03:30:36 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -3,19 +3,20 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init() {
-  function onKeyDownHandler(e) {
-    const key = parseInt(e.detail || e.which);
+  document.body.addEventListener('keydown', onKeyDownHandler)
+// function onKeyDownHandler(e) {
+  //  const key = parseInt(e.detail || e.which);
 
-    if (key === alphabet[index]) {
-      index++;
+//    if (key === alphabet[index]) {
+  //    index++;
 
-      if (index === alphabet.length) {
-        alert("Hurray!");
+  //    if (index === alphabet.length) {
+    //    alert("Hurray!");
 
-        index = 0;
-      }
-    } else {
-      index = 0;
-    }
-  }
+    //    index = 0;
+//      }
+//    } else {
+  //    index = 0;
+//    }
+//  }
 }
