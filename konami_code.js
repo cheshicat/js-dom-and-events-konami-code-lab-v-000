3214fe7b7e0737a28d2dcebf1e0fd77d1d327f0b
Allison Howard <alliemhow@gmail.com>
2019-09-05 03:37:22 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -7,16 +7,16 @@ function init() {
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
 
-//    if (key === alphabet[index]) {
-  //    index++;
+    if (key === code[index]) {
+      index++;
 
-  //    if (index === alphabet.length) {
-    //    alert("Hurray!");
+      if (index === code.length) {
+        alert("Hurray!");
 
-    //    index = 0;
-//      }
-//    } else {
-  //    index = 0;
-//    }
+        index = 0;
+        }
+      } else {
+     index = 0;
+    }
   }
 }
